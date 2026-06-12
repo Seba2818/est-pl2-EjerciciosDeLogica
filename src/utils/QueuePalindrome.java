b5f7aae9f09fdbb48d7919957ff4d1e4bedfed2a
Your Name <you@example.com>
2026-06-12 10:34:58 -0500

    u

--- src/utils/QueuePalindrome.java
+++ src/utils/QueuePalindrome.java
@@ -1,5 +1,42 @@
 package utils;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class QueuePalindrome {
-    
+
+
+    public boolean isPalindrome(String text) {
+
+
+        Queue<Character> queue = new LinkedList<>();
+
+
+        for(char c : text.toLowerCase().toCharArray()){
+            queue.add(c);
+        }
+
+        int size = queue.size();
+
+        for(int i = 0; i < size / 2; i++){
+
+            char first = queue.remove();
+            Queue<Character> temp = new LinkedList<>();
+
+            while(queue.size() > 1){
+                temp.add(queue.remove());
+            }
+
+            char last = queue.remove();
+
+            while(!temp.isEmpty()){
+                queue.add(temp.remove());
+            }
+
+            if(first != last){
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- src/utils/SignValidator.java
+++ src/utils/SignValidator.java
@@ -1,5 +1,40 @@
 package utils;
 
+import java.util.Stack;
+
 public class SignValidator {
-    
+    public boolean isValid(String s) {
+
+        Stack<Character> pila = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char c = s.charAt(i);
+
+            // Aperturas
+            if (c == '(' || c == '{' || c == '[') {
+                pila.push(c);
+            }
+
+            // Cierres
+            else if (c == ')' || c == '}' ||  c == ']') {
+
+                if (pila.isEmpty()) {
+                    return false;
+                }
+
+                char tope = pila.pop();
+
+                if ((c == ')' && tope != '(') ||
+                    (c == '}' && tope != '{') ||
+                    (c == ']' && tope != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        return pila.isEmpty();
+    }
 }
+    
+
